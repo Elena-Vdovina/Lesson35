2eb67af6c090e01ab28d265c8379000618f6829b
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-07 00:01:31 +0100

    Add class Student

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ public class Main {
     //23.1
     //1 Создать класс Student c полями (переменными) имя и номер.
     //  Создать несколько объектов этого класса
+    Student ivanov = new Student();
+    ivanov.name = "Иванов Иван";
+    ivanov.number = 1;
+    ivanov.checkTest();
 
+    Student petrov = new Student();
+    petrov.name = "Петров Петр";
+    petrov.number = 2;
+    petrov.checkTest();
 
     //2 Добавить в класс Student конструктор с параметрами имя и номер.
     //  Создать еще несколько объектов класса с использованием этого конструктора
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+  String name;
+  int number;
+
+  void checkTest() {
+    System.out.println(this.name + " test checked");
+  }
+
+}
