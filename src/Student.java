8c50f185874257b88cd5b6c7d5693bdc048c7ef7
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-07 00:05:48 +0100

    Add constructors

--- src/Student.java
+++ src/Student.java
@@ -2,6 +2,15 @@ public class Student {
   String name;
   int number;
 
+  Student() {
+
+  }
+
+  Student(String name, int number) {
+    this.name = name;
+    this.number = number;
+  }
+
   void checkTest() {
     System.out.println(this.name + " test checked");
   }
